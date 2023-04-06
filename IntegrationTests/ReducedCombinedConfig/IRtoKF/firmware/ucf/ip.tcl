bc4a57c08a63aaae499e98c831be7db42019c577
FilMarini <marinifil@gmail.com>
2023-04-06 18:03:53 +0200

    fixed for CM

--- IntegrationTests/ReducedCombinedConfig/IRtoKF/firmware/ucf/ip.tcl
+++ IntegrationTests/ReducedCombinedConfig/IRtoKF/firmware/ucf/ip.tcl
@@ -1,2 +1,2 @@
-set_property ip_repo_paths  ../../../src/firmware-hls/IntegrationTests/ReducedConfig/IRtoKF/firmware/cgn/ [current_project]
+set_property ip_repo_paths  ../../../src/firmware-hls/IntegrationTests/ReducedCombinedConfig/IRtoKF/firmware/cgn/ [current_project]
 update_ip_catalog
