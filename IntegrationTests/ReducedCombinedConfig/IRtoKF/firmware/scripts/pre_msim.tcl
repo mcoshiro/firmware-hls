49d94b70327d835bac117477bde3661389539469
FilMarini <marinifil@gmail.com>
2023-04-13 10:24:44 +0200

    fixed oversight for CM

--- IntegrationTests/ReducedCombinedConfig/IRtoKF/firmware/scripts/pre_msim.tcl
+++ IntegrationTests/ReducedCombinedConfig/IRtoKF/firmware/scripts/pre_msim.tcl
@@ -1,3 +1,3 @@
 # Copy LUTs to xsim or synth_1 directory
 file delete -force ./LUTs
-file copy -force ../../../../../../../src/firmware-hls/emData/LUTsReduced ./LUTs
+file copy -force ../../../../../../../src/firmware-hls/emData/LUTsCMReduced ./LUTs
